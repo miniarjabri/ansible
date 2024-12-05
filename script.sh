1011619a3471e914a1313b5c885614c5b9ebb55f
Miniar JABRI <123066240+miniarjabri@users.noreply.github.com>
2024-12-05 16:00:04 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-mkdir -p roles/base/tasks
-mkdir -p roles/db_servers/tasks
-mkdir -p roles/file_servers/tasks
-mkdir -p roles/web_servers/tasks
-mkdir -p roles/workstations/tasks
+# Liste des rôles
+roles=("base" "db_servers" "file_servers" "web_servers" "workstations")
+
+# Création des répertoires et fichiers
+mkdir -p roles
+for role in "${roles[@]}"; do
+    mkdir -p roles/$role/tasks
+    touch roles/$role/tasks/main.yml
+    echo "# Main tasks for $role" > roles/$role/tasks/main.yml
+done
+
+echo "Structure des rôles et fichiers main.yml créée avec succès !"
